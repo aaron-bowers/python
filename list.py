67408c698dab887df427767ba322c35d52a4c919
Aaron Bowers <bowersaaronjames@gmail.com>
2022-08-24 16:30:34 -0700

    list research

---
+++ list.py
@@ -0,0 +1,17 @@
+# LIST - stores multiple items in a single variable
+"""
+  1) defined ordered with indexes starting at 0
+  2) changeable meaning add and remove items
+  3) allow duplicate values
+  4) allow differnt data types
+"""
+
+thislist = ['apple', 'banana', 'cherry']
+print(thislist)
+print(type(thislist)) # <class 'list'>
+
+# LIST METHODS   -   https://www.w3schools.com/python/python_lists_methods.asp
+
+# list() CONSTRUCTOR
+thislist = list(('apple', 'banana', 'cherry'))
+print(thislist)
--- operators.py
+++ operators.py
@@ -1,15 +1,52 @@
-# OPERATORS
+# ARITHMETIC OPERATORS
+  # ADDITION +
+  # SUBTRACTION -
+  # MULTIPLICATION *
+  # DIVISION /
+  # MODULUS %
+  # EXPONENTIATION **
+  # FLOOR DIVISION // - rounds down to the nearest whole number
 
-# ADDITION +
+# ASSIGNMENT OPERATORS
+  # =
+  # +=
+  # -=
+  # *=
+  # /=
+  # %=
+  # //=
+  # **=
+  # &=
+  # |=
+  # ^=
+  # >>
+  # <<
 
-# SUBTRACTION -
+# COMPARISON OPERATORS
+  # ==
+  # !=
+  # >
+  # <
+  # >=
+  # <=
 
-# MULTIPLICATION *
+# LOGICAL OPERATORS
+  # and - returns True if both statements are True
+  # or - returns True if one of the statements is True
+  # not - reverses the result (False if returns true)
 
-# DIVISION /
+# IDENTITY OPERATORS
+  # is - returns True if both variables refer to the same object in memory
+  # is not - returns True if both variables do not refer to the same object in memory
 
-# MODULUS %
+# MEMBORSHIP OPERATORS
+  # in - returns True if a specific value is present within a sequence
+  # not in - visa versa
 
-# EXPONENTIATION **
-
-# FLOOR DIVISION //
+# BITWISE OPERATORS
+  # & - AND - Sets each bit to 1 if both bits are 1
+  # | - OR - Sets each bit to 1 if one of two bits are 1
+  # ^ - XOR - Sets each bit to 1 if only one of two bits are 1
+  # ~ - NOT - Inverts all the bits
+  # << - Zero fill left shift - Shift left by pushing zeros in from the right and let the left-most bits fall off
+  # >> - Zero fill right shift - Shift right by pushing zeros in from the left and let the right-most bits fall off
