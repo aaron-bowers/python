bdfd752174f607f34157f563422530132eba8314
Aaron Bowers <bowersaaronjames@gmail.com>
2022-08-24 09:57:01 -0700

    python studies

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+print("Hello, World!")
+
+# indentation is required for new statements
+if 5 > 5:
+    print('Five is greater than five')
+else:
+    print('Woops, what was that calculation')
---
+++ variables.py
@@ -0,0 +1,31 @@
+# variable are created when assigned a value
+x = 5
+y = "Hello, World!"
+
+"""
+this works as a block of comments
+"""
+
+"""
+CREATING VARIABLES
+"""
+
+# do not need to be declared with any particular type, and can change after being set
+x = 4
+print(x)
+x = "Sally"
+print(x)
+
+# CASTING - specifying the data type of a variable
+x = str(3)
+y = int(3)
+z = float(3)
+print(x, type(x), y, type(y), z, type(z))
+
+# single or double quotes is acceptable
+x = "John"
+y = 'John'
+
+# variable names are case sensitive
+a = 4
+A = "Sally"
