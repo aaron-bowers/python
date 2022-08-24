f4e7a7d48cd82eaeb8b76271740ca808bb031ff1
Aaron Bowers <bowersaaronjames@gmail.com>
2022-08-24 10:50:27 -0700

    review casting

--- casting.py
+++ casting.py
@@ -1,8 +1,22 @@
 # As an object-oriented language, Python uses classes to define data types, including primitive types
 
-# types of constructor functions
-int() # constructs an integer number from an integer literal, a float literal, or a string literal
+# TYPES OF CONSTRUCTOR FUNCTIONS
 
+int() # constructs an integer number from an integer literal, a float literal, or a string literal (which must be a whole number)
 print(int(5))
 print(int(5.0505))
-print(int('five'))
+print(int('5'))
+
+
+
+float() # constructs a float number from an int, float, or str
+print(float(1))
+print(float(2.8))
+print(float("3"))
+print(float("3.2"))
+
+
+str() # constructs a string from a wide variety of data types, including strings, int, and float
+print(str('s1'))
+print(str(5))
+print(str(6.01))
---
+++ strings.py
@@ -0,0 +1,3 @@
+#  STRINGS are surronded by either single or double quotation marks
+
+print("Hello", " AND ", 'Hello', " ARE THE SAME.")
