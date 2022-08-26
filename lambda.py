8af5755924c339c95e0d69961782fe691b30fe4e
Aaron Bowers <bowersaaronjames@gmail.com>
2022-08-26 14:48:43 -0700

    learning lambdas

--- lambda.py
+++ lambda.py
@@ -7,3 +7,16 @@
 # Add 10 to arg a, and return the result
 x = lambda a : a + 10
 print(x(5))
+
+# take any num of arguments
+x = lambda a, b : a * b
+print(x(5,6))
+
+# return a lambda function from within another function
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+print(mydoubler(5))
+mytrippler = myfunc(3)
+print(mytrippler(5))
