a9e3cd77cf68ea36aa853e37d7895fe4bb231861
Aaron Bowers <bowersaaronjames@gmail.com>
2022-08-26 10:21:51 -0700

    initialize lambda file

---
+++ lambda.py
@@ -0,0 +1,9 @@
+# LAMBDA - small anonymous function
+'''
+  - takes any num of args, but can only have one expression
+  - SYNTAX  -->  lambda arguments : expression
+'''
+
+# Add 10 to arg a, and return the result
+x = lambda a : a + 10
+print(x(5))
--- recursion.py
+++ recursion.py
@@ -1,2 +1,25 @@
 # RECURSION - defined function invokes itself until it reaches some condition (base case)
 
+# tri_recursion() example
+def tri_recursion(k):
+  if k > 0:
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("Recursion Example Results")
+tri_recursion(6)
+
+# calculate factorials
+def tri_recursion_factorial(k):
+  if k > 1:
+    result = k * tri_recursion_factorial(k - 1)
+    print(result)
+  else:
+    result = 1
+  return result
+
+print("Recursion Factorial Example Results")
+tri_recursion_factorial(5)
